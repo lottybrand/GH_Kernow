150535d0d1ebcabd4e97b38b316c6b6393825821
Charlotte Brand <git>
2018-03-22 13:12:47 +0000

    finally sorted the NA rubbish, I think. Cronbach's alpha worryingly low... need to look into this! Check right items have been reversed, check no lurking "999s" left instead of "nas" etc.

--- KernowAnalysis.R
+++ KernowAnalysis.R
@@ -1,8 +1,9 @@
 ######## Kernow Study Analysis
 library(dplyr)
+library(psy)
 setwd("~/Desktop/Postdoc/CornwallCommunityStudy/results/Kernow/")
 
-pdRatings <- read.delim("kernow_results_16_03_18_pds.txt")
+pdRatings <- read.csv("kernowResults220318.csv")
 
 ### Calculate p&d scores
 ### Reverse score items 2,6,10,12,17 as according to Cheng et al. 2013 (should really write a function for this..!!)
@@ -13,21 +14,21 @@ pdRatings$R2rev <- ifelse((pdRatings$R2==1),7,
                                         ifelse((pdRatings$R2==4),4,
                                                ifelse((pdRatings$R2==5),3,
                                                       ifelse((pdRatings$R2==6),2,
-                                                             ifelse((pdRatings$R2==7),1,"NA")))))))
+                                                             ifelse((pdRatings$R2==7),1,NA)))))))
 pdRatings$R6rev <- ifelse((pdRatings$R6==1),7,
                           ifelse((pdRatings$R6==2),6,
                                  ifelse((pdRatings$R6==3),5,
                                         ifelse((pdRatings$R6==4),4,
                                                ifelse((pdRatings$R6==5),3,
                                                       ifelse((pdRatings$R6==6),2,
-                                                             ifelse((pdRatings$R6==7),1,"NA")))))))
+                                                             ifelse((pdRatings$R6==7),1,NA)))))))
 pdRatings$R10rev <- ifelse((pdRatings$R10==1),7,
                           ifelse((pdRatings$R10==2),6,
                                  ifelse((pdRatings$R10==3),5,
                                         ifelse((pdRatings$R10==4),4,
                                                ifelse((pdRatings$R10==5),3,
                                                       ifelse((pdRatings$R10==6),2,
-                                                             ifelse((pdRatings$R10==7),1,"NA")))))))
+                                                             ifelse((pdRatings$R10==7),1,NA)))))))
 
 pdRatings$R12rev <- ifelse((pdRatings$R12==1),7,
                           ifelse((pdRatings$R12==2),6,
@@ -35,7 +36,7 @@ pdRatings$R12rev <- ifelse((pdRatings$R12==1),7,
                                         ifelse((pdRatings$R12==4),4,
                                                ifelse((pdRatings$R12==5),3,
                                                       ifelse((pdRatings$R12==6),2,
-                                                             ifelse((pdRatings$R12==7),1,"NA")))))))
+                                                             ifelse((pdRatings$R12==7),1,NA)))))))
 
 pdRatings$R17rev <- ifelse((pdRatings$R17==1),7,
                           ifelse((pdRatings$R17==2),6,
@@ -43,14 +44,25 @@ pdRatings$R17rev <- ifelse((pdRatings$R17==1),7,
                                         ifelse((pdRatings$R17==4),4,
                                                ifelse((pdRatings$R17==5),3,
                                                       ifelse((pdRatings$R17==6),2,
-                                                             ifelse((pdRatings$R17==7),1,"NA")))))))
+                                                             ifelse((pdRatings$R17==7),1,NA)))))))
+
+write.csv(pdRatings, file="pdRatings.csv")
+
+#something fishy happening when trying to get the means, argggggh
+#pdRatings$prestigeMean <- ave(pdRatings$R1,pdRatings$R2rev, na.rm=TRUE)
+#pdRatings$prestigeMean <- rowMeans(pdRatings[,c("R1","R2rev","R4")], na.rm = TRUE)
+#try the dplyr way?? 
+#pdRatings %>%
+#  rowwise() %>%
+#  mutate(c=mean(c(R1,R2rev,R4)))
+
+#cronbach test stuff
+PrestigeRatings <- subset(pdRatings, select = c(R1, R2rev, R4, R6rev, R8, R13, R14, R15, R17rev))
+DominanceRatings <- subset(pdRatings, select = c(R3, R5, R7, R9, R10rev, R11, R12rev, R16))
+#Pratings <- na.omit(PrestigeRatings)
+#let's see if it's to do with nas
+#PratingsNA <- Pratings[1:3,]
+cronbach(PrestigeRatings)
+cronbach(DominanceRatings)
 
 
-pdRatings$prestigeMean <- ave(pdRatings$R1,pdRatings$R2rev, na.rm=TRUE)
-
-pdRatings$prestigeMean <- rowMeans(pdRatings[,c("R1","R2rev","R4")], na.rm = TRUE)
-
-
-pdRatings %>%
-  rowwise() %>%
-  mutate(c=mean(c(R1,R2rev,R4)))
