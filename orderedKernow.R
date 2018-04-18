1772cb589e1a6115a3d566f92edc056193956bf8
Charlotte Brand <git>
2018-04-18 17:09:17 +0100

    reshaped the data. Now have two prestige and dominance dataframes (pdSubLong and pdSubLong2). Should be able to work with these for ordinal models now...

--- orderedKernow.R
+++ orderedKernow.R
@@ -19,7 +19,28 @@ colnames(pdSub)
 prestige <- c("R1", "R2rev", "R4", "R6rev", "R8", "R13", "R14", "R15", "R17rev")
 ?reshape
 pdSubLong <- reshape(pdSub, times = prestige,
-                       idvar = "rated_ID", 
-                          varying = list(c()),
-                          v.names = c("prestige"), 
-                          direction = "long")
+                     varying = prestige,
+                     v.names = c("prestigeRatings"), 
+                     direction = "long")
+
+pdSubLong <- pdSubLong[order(pdSubLong$Group),]
+colnames(pdSubLong)[17] <- "presItem"
+
+dominance <- c("R3", "R5", "R7", "R9", "R10rev", "R11", "R12rev", "R16")
+
+#hmmm this didn't work with pdSubLong instead of pdSub... 
+pdSubLong2 <- reshape(pdSub, times = dominance,
+                      varying = dominance,
+                      v.names = c("dominanceRatings"),
+                      direction = "long")
+
+#now two diff datasets created, one with the prestige ratings, another with the dominance. Maybe that's fine...
+#delete unwated bits:
+
+colnames(pdSubLong)
+pdSubLong[,4:16] <- NULL
+
+colnames(pdSubLong2)
+pdSubLong2[,4:17] <- NULL
+
+#cool, should be able to do some ordinal with these 
