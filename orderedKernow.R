08e0ce747577e3c7fd4c7df6a0039495a70a7b81
Charlotte Brand <git>
2018-04-20 17:45:23 +0100

    full lot of prestige and dominance ordinal models. want to check and compare estimates of nominations too, that should be the final two models. also posterior predictions obviously, and postcheck.

--- orderedKernow.R
+++ orderedKernow.R
@@ -54,6 +54,7 @@ presPlot <- simplehist(gPresD$prestigeRatings, xlim = c(1,7), xlab = "response")
 domPlot <- simplehist(gDomD$dominanceRatings, xlim = c(1,7), xlab = "response")
 domPlot
 
+hist(kernowResults$Age, xlim = c(0,100), ylim = c(0,40), xlab = "Age")
 
 #okay let's try and add the other info in from the other datasets for predictors.
 #for Full prestige model, we need, score, overconfidence, influence, likeability, sex, age. 
@@ -73,16 +74,21 @@ gPresD$initLrn <- kernowResults$intial_learn[match(gPresD$rated_ID, kernowResult
 gPresD$Liked <- kernowResults$aveLik[match(gPresD$rated_ID, kernowResults$ID)]
 gPresD$Inflntl <- kernowResults$aveInf[match(gPresD$rated_ID, kernowResults$ID)]
 
+#centre score & overconf
+gPresD$ScoreC <- gPresD$Score - mean(gPresD$Score)
+gPresD$overconf <- gPresD$OverConf+40
+meanOConf <- mean(gPresD$overconf, na.rm = TRUE)
+gPresD$OverConfC <- gPresD$overconf - meanOConf
 
+gPresD_NA <- na.omit(gPresD)
 
 #coerce_index 
-gPresD$raterId <- coerce_index(gPresD$rater_id)
-gPresD$ratedID <- coerce_index(gPresD$rated_ID)
-gPresD$grpID <- coerce_index(gPresD$Group)
-gPresD$itemID <- coerce_index(gPresD$presItem)
+gPresD_NA$raterId <- coerce_index(gPresD_NA$rater_id)
+gPresD_NA$ratedID <- coerce_index(gPresD_NA$rated_ID)
+gPresD_NA$grpID <- coerce_index(gPresD_NA$Group)
+gPresD_NA$itemID <- coerce_index(gPresD_NA$presItem)
 
-#centre score:
-gPresD$ScoreC <- gPresD$Score - mean(gPresD$Score)
+
 
 #Null
 pM_null <- map2stan(
@@ -97,7 +103,7 @@ pM_null <- map2stan(
     c(sigmaID, sigmaR, sigmaG, sigmaItem) ~ dcauchy(0,1),
     cutpoints ~ dnorm(0,10)
   ),
-  data=gPresD, 
+  data=gPresD_NA, 
   constraints = list(sigmaID = "lower=0", sigmaR = "lower=0", sigmaG = "lower=0", sigmaItem = "lower=0"),
   start = list(cutpoints=c(-2,-1,0,1,2,2.5)),
   chains = 1, cores = 1)
@@ -120,7 +126,7 @@ pM_exp <- map2stan(
     c(sigmaID, sigmaR, sigmaG, sigmaItem) ~ dcauchy(0,1),
     cutpoints ~ dnorm(0,10)
   ),
-  data=gPresD, 
+  data=gPresD_NA, 
   constraints = list(sigmaID = "lower=0", sigmaR = "lower=0", sigmaG = "lower=0", sigmaItem = "lower=0"),
   start = list(cutpoints=c(-2,-1,0,1,2,2.5)),
   chains = 1, cores = 1)
@@ -142,24 +148,31 @@ pM_Apriori <- map2stan(
     c(sigmaID, sigmaR, sigmaG, sigmaItem) ~ dcauchy(0,1),
     cutpoints ~ dnorm(0,10)
   ),
-  data=gPresD, 
+  data=gPresD_NA, 
   constraints = list(sigmaID = "lower=0", sigmaR = "lower=0", sigmaG = "lower=0", sigmaItem = "lower=0"),
   start = list(cutpoints=c(-2,-1,0,1,2,2.5)),
   chains = 1, cores = 1)
 
 precis(pM_Apriori)
-
-gPresD_NA <- na.omit(gPresD)
+plot(precis(pM_Apriori))
+#Mean StdDev lower 0.89 upper 0.89 n_eff Rhat
+#bS        -0.02   0.02      -0.05       0.01   279 1.00
+#bI         3.24   0.41       2.61       3.90   291 1.00
+#bL         7.12   0.70       6.04       8.23   242 1.00
+#sigmaID    0.53   0.06       0.44       0.61   359 1.00
+#sigmaR     1.07   0.08       0.92       1.18   228 1.00
+#sigmaG     0.18   0.12       0.00       0.34    84 1.05
+#sigmaItem  1.30   0.36       0.79       1.79   217 1.0
 
 #try full multi level: 
 pMFull <- map2stan(
   alist(
     prestigeRatings ~ dordlogit(phi, cutpoints),
-    phi <- bS*ScoreC + bI*Inflntl + bL*Liked + bN*nominated + 
+    phi <- bS*ScoreC + b_o*OverConf + bI*Inflntl + bL*Liked + bN*nominated + 
       bIn*initInf + bInl*initLrn + bSx*Sex + bA*Age +
       aID[ratedID]*sigmaID + aR[raterId]*sigmaR +
       aG[grpID]*sigmaG + aItem[itemID]*sigmaItem,
-    c(bS, bI, bL, bN, bIn, bInl, bSx, bA) ~ dnorm(0,10),
+    c(bS, b_o, bI, bL, bN, bIn, bInl, bSx, bA) ~ dnorm(0,10),
     aG[grpID] ~ dnorm(0,1),
     aID[ratedID]  ~ dnorm(0,1),
     aR[raterId] ~ dnorm(0,1),
@@ -175,6 +188,13 @@ pMFull <- map2stan(
 precis(pMFull)
 plot(precis(pMFull))
 compare(pM_null, pM_Apriori, pM_exp, pMFull, refresh=0.1)
+#WAIC pWAIC dWAIC weight     SE   dSE
+#pM_Apriori 11034.1 201.9   0.0   0.81 123.54    NA
+#pMFull     11037.0 202.8   2.8   0.19 123.97  4.43
+#pM_null    11054.7 224.5  20.6   0.00 123.63 10.64
+#pM_exp     11058.2 224.9  24.0   0.00 123.88 10.89
+
+
 
 ############################
 ############################
@@ -207,16 +227,19 @@ gDomD$Liked <- kernowResults$aveLik[match(gDomD$rated_ID, kernowResults$ID)]
 gDomD$Inflntl <- kernowResults$aveInf[match(gDomD$rated_ID, kernowResults$ID)]
 
 
+#centre score & over confidence:
+gDomD$ScoreC <- gDomD$Score - mean(gDomD$Score)
+gDomD$overconf <- gDomD$OverConf+40
+meanOConf <- mean(gDomD$overconf, na.rm = TRUE)
+gDomD$OverConfC <- gDomD$overconf - meanOConf
+
+gDomD_NA <- na.omit(gDomD)
 
 #coerce_index 
-gDomD$raterId <- coerce_index(gDomD$rater_id)
-gDomD$ratedID <- coerce_index(gDomD$rated_ID)
-gDomD$grpID <- coerce_index(gDomD$Group)
-gDomD$itemID <- coerce_index(gDomD$domItem)
-
-#centre score:
-gDomD$ScoreC <- gDomD$Score - mean(gDomD$Score)
-gDomD$OverConfC <- gDomD$OverConf - mean(gDomD$OverConf)
+gDomD_NA$raterId <- coerce_index(gDomD_NA$rater_id)
+gDomD_NA$ratedID <- coerce_index(gDomD_NA$rated_ID)
+gDomD_NA$grpID <- coerce_index(gDomD_NA$Group)
+gDomD_NA$itemID <- coerce_index(gDomD_NA$domItem)
 
 #Null
 dM_null <- map2stan(
@@ -231,7 +254,7 @@ dM_null <- map2stan(
     c(sigmaID, sigmaR, sigmaG, sigmaItem) ~ dcauchy(0,1),
     cutpoints ~ dnorm(0,10)
   ),
-  data=gDomD, 
+  data=gDomD_NA, 
   constraints = list(sigmaID = "lower=0", sigmaR = "lower=0", sigmaG = "lower=0", sigmaItem = "lower=0"),
   start = list(cutpoints=c(-2,-1,0,1,2,2.5)),
   chains = 1, cores = 1)
@@ -242,20 +265,95 @@ precis(dM_null)
 dM_aPriori <- map2stan(
   alist(
     dominanceRatings ~ dordlogit(phi, cutpoints),
-    phi <- bO*OverConfC + bI*Inflntl +
+    phi <- b_o*OverConfC + bI*Inflntl +
       aID[ratedID]*sigmaID + aR[raterId]*sigmaR +
       aG[grpID]*sigmaG + aItem[itemID]*sigmaItem,
     aG[grpID] ~ dnorm(0,1),
     aID[ratedID]  ~ dnorm(0,1),
     aR[raterId] ~ dnorm(0,1),
     aItem[itemID] ~ dnorm(0,1),
-    c(bO, bI) ~ dnorm(0,1),
+    c(b_o, bI) ~ dnorm(0,1),
     c(sigmaID, sigmaR, sigmaG, sigmaItem) ~ dcauchy(0,1),
     cutpoints ~ dnorm(0,10)
   ),
-  data=gDomD, 
+  data=gDomD_NA, 
   constraints = list(sigmaID = "lower=0", sigmaR = "lower=0", sigmaG = "lower=0", sigmaItem = "lower=0"),
   start = list(cutpoints=c(-2,-1,0,1,2,2.5)),
   chains = 1, cores = 1)
 
 precis(dM_aPriori)
+plot(precis(dM_aPriori))
+
+# initial ratings? (Expl)
+
+dM_exp <- map2stan(
+  alist(
+    dominanceRatings ~ dordlogit(phi, cutpoints),
+    phi <-  bIn*initInf + bInl*initLrn +
+      aID[ratedID]*sigmaID + aR[raterId]*sigmaR +
+      aG[grpID]*sigmaG + aItem[itemID]*sigmaItem,
+    c(bIn, bInl) ~ dnorm(0,10),
+    aG[grpID] ~ dnorm(0,1),
+    aID[ratedID]  ~ dnorm(0,1),
+    aR[raterId] ~ dnorm(0,1),
+    aItem[itemID] ~ dnorm(0,1),
+    c(sigmaID, sigmaR, sigmaG, sigmaItem) ~ dcauchy(0,1),
+    cutpoints ~ dnorm(0,10)
+  ),
+  data=gDomD_NA, 
+  constraints = list(sigmaID = "lower=0", sigmaR = "lower=0", sigmaG = "lower=0", sigmaItem = "lower=0"),
+  start = list(cutpoints=c(-2,-1,0,1,2,2.5)),
+  chains = 1, cores = 1)
+
+precis(dM_exp)
+
+#Mean StdDev lower 0.89 upper 0.89 n_eff Rhat
+#bIn       0.72   0.30       0.24       1.20  1000    1
+#bInl      0.13   0.42      -0.56       0.77  1000    1
+#sigmaID   1.10   0.09       0.97       1.24   424    1
+#sigmaR    1.08   0.09       0.94       1.22   405    1
+#sigmaG    0.19   0.15       0.00       0.40   134    1
+#sigmaItem 1.60   0.45       0.94       2.17   501    1
+
+dM_FULL <- map2stan(
+  alist(
+    dominanceRatings ~ dordlogit(phi, cutpoints),
+    phi <- bs*Score + b_o*OverConfC + bI*Inflntl + bL*Liked + b_n*nominated +
+      bIn*initInf + bInlrn*initLrn + bsx*Sex + ba*Age +
+      aID[ratedID]*sigmaID + aR[raterId]*sigmaR +
+      aG[grpID]*sigmaG + aItem[itemID]*sigmaItem,
+    aG[grpID] ~ dnorm(0,1),
+    aID[ratedID]  ~ dnorm(0,1),
+    aR[raterId] ~ dnorm(0,1),
+    aItem[itemID] ~ dnorm(0,1),
+    c(bs, b_o, bI, bL, bIn, bInlrn, bsx, ba, b_n) ~ dnorm(0,1),
+    c(sigmaID, sigmaR, sigmaG, sigmaItem) ~ dcauchy(0,1),
+    cutpoints ~ dnorm(0,10)
+  ),
+  data=gDomD_NA, 
+  constraints = list(sigmaID = "lower=0", sigmaR = "lower=0", sigmaG = "lower=0", sigmaItem = "lower=0"),
+  start = list(cutpoints=c(-2,-1,0,1,2,2.5)),
+  chains = 1, cores = 1)
+
+precis(dM_FULL)
+plot(precis(dM_FULL))
+#Mean StdDev lower 0.89 upper 0.89 n_eff Rhat
+#bs        -0.01   0.03      -0.05       0.03   877    1
+#b_o        0.02   0.01       0.00       0.05  1000    1
+#bI         2.31   0.57       1.30       3.13   810    1
+#bL        -2.29   0.75      -3.41      -1.04  1000    1
+#bIn        0.55   0.26       0.15       0.95  1000    1
+#bInlrn    -0.11   0.35      -0.67       0.43  1000    1
+#bsx        0.27   0.22      -0.12       0.57  1000    1
+#ba         0.02   0.01       0.00       0.02   591    1
+#sigmaID    0.90   0.08       0.78       1.04   253    1
+#sigmaR     1.09   0.08       0.98       1.24   520    1
+#sigmaG     0.24   0.18       0.00       0.48   110    1
+#sigmaItem  1.60   0.48       0.99       2.26   559    1
+
+compare(dM_null, dM_aPriori, dM_exp, dM_FULL)
+#WAIC pWAIC dWAIC weight     SE  dSE
+#dM_FULL    10374.7 228.7   0.0   0.78 114.56   NA
+#dM_aPriori 10379.1 231.5   4.4   0.09 114.51 3.28
+#dM_exp     10379.2 231.8   4.5   0.08 114.68 4.01
+#dM_null    10380.1 232.8   5.4   0.05 114.63 4.62
